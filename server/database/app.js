bb6f1d740a8333c648b7b2173cf6319b12a73a0a
Andreea Popa <andreea13.popa@yahoo.com>
2026-07-30 08:33:28 -0400

    Implement dealer reviews and sentiment integration

--- server/database/app.js
+++ server/database/app.js
@@ -76,15 +76,14 @@ app.get('/fetchDealers/:state', async (req, res) => {
     }
   });
 
-// Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
     try {
-      const document = await Dealerships.findOne({ id: req.params.id });
-      res.json(document);
+        const documents = await Dealerships.find({ id: parseInt(req.params.id) });
+        res.json(documents);
     } catch (error) {
-      res.status(500).json({ error: 'Error fetching documents' });
+        res.status(500).json({ error: 'Error fetching documents' });
     }
-  });
+});
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
